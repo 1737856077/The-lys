b9c276b62e533f13948a0b04b6324d7dd7eb23d9
EDZ-20180516PBB\Edianzu <814418177@qq.com>
2019-03-07 14:39:32 +0800

    增加带经纬度的地区信息

--- home/index/controller/Index.php
+++ home/index/controller/Index.php
@@ -14,94 +14,9 @@ use think\Session;
 
 class Index extends  Controller
 {
-
     public function index(){
-
-        if(Session::has('adminname')){
-            return $this->fetch();
-        }else{
-            $this->redirect('login',"",1,"请登录，1称后自动跳转到登录页面");
-        }
+        echo "<script language=\"javascript\">window.location.href='http://www.kedousuyuan.com/';</script>";
+        exit;
     }
-
-    public function main(){
-        return $this->fetch();
-
-    }
-
-    public function header(){
-        return $this->fetch();
-    }
-
-    public function menu(){
-        return $this->fetch();
-    }
-
-    /*
-     * 用户登录
-     */
-    public function login(){
-
-        if(Session::has('adminname')){
-            //$this->redirect("","",1,"已登录，1称后自动跳转");
-            echo "<script language=\"javascript\">window.open('".url('index/index')."','_top');</script>";
-        }else{
-            return $this->fetch();
-        }
-    }
-
-    public function checklogin(){
-        $adminname=htmlspecialchars(trim($_POST['adminname']));
-        $adminpwd=$_POST['adminpwd'];
-
-        $map=array();
-        $map['name']=array('eq',$adminname);
-        $map['pwd']=array('eq',md5($adminpwd));
-        $ModelAdmin=Db::name('admin');
-        $getoneAdmin=$ModelAdmin->where($map)->find();
-
-        if(empty($getoneAdmin)){
-            $this->error('用户名或密码错误!');
-        }else{
-            Session::set('adminid', $getoneAdmin["admin_id"]);
-            Session::set('adminname', $getoneAdmin["name"]);
-            Session::set('adminoskey', $getoneAdmin["oskey"]);
-            Session::set('admin_permissions', $getoneAdmin["permissions"]);
-            //添加日志 begin
-            $_content=$getoneAdmin['name'].'登录网站后台管理系统。';
-            $ModelAdminOperateLog=Db::name('admin_operate_log');
-            $dataAdminOperateLog=array("content"=>$_content,
-                                       "admin_id"=>Session::get('adminid'),
-                                       "create_ip"=>$_SERVER["REMOTE_ADDR"],
-                                       "create_time"=>time(),
-            );
-            $ModelAdminOperateLog->insert($dataAdminOperateLog);
-
-            //添加日志 end
-            echo "<script language=\"javascript\">window.open('".url('index/index')."','_top');</script>";
-            //$this->redirect("","",1,"欢迎 ".$tmpname." 登录，1称后自动跳转");
-        }
-    }
-
-    public function unlogin(){
-        //Session::clear();
-        //添加日志 begin
-        $_content=Session::get('adminname').'退出网站后台管理系统。';
-        $ModelAdminOperateLog=Db::name('admin_operate_log');
-        $dataAdminOperateLog=array("content"=>$_content,
-                                   "admin_id"=>Session::get('adminid'),
-                                   "create_ip"=>$_SERVER["REMOTE_ADDR"],
-                                   "create_time"=>time(),
-        );
-        $ModelAdminOperateLog->insert($dataAdminOperateLog);
-        //添加日志 end
-        Session::delete('adminid');
-        Session::delete('adminname');
-        Session::delete('adminoskey');
-
-        echo "<script language=\"javascript\">window.open('".url('index/index')."','_top');</script>";
-    }
-
-
 }
 
