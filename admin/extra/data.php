766694be59971a17d81f8cf1e8627f111eeb1846
EDZ-20180516PBB\Edianzu <814418177@qq.com>
2019-03-27 17:13:25 +0800

    提交在线下单测试文件

--- admin/extra/data.php
+++ admin/extra/data.php
@@ -21,6 +21,10 @@ return [
         'savename_rule'=>'date',
         'excel_backup'=>'./runtime/temp/',
     ],
+	'alipay' => [
+		//该地址表示用户缴费支付完成后开发者系统接受支付结果通知的回调地址。
+    	'notify_url' => 'https://yunpai.sindns.com/index.php/alipay/wypaycallback/index',
+	],
 
     //二维码配置信息
     'qr_data'=>['query_url'=>'https://zhuisu.sindns.com/index.php/index/product/index',//二维码的查询地址
--- home/config.php
+++ home/config.php
@@ -10,4 +10,27 @@ $newArray=require_once('config.php');
 $newArray['app_debug']=true;// 应用调试模式
 $newArray['app_trace']=true; // 应用Trace
 $newArray['app_status']=''; // 应用模式状态
+
+// +----------------------------------------------------------------------
+// | 网站基本信息配置  begin
+// +----------------------------------------------------------------------
+$newArray['custom_config']=[
+    'WebConfig_Name'=>'云标签',
+    'WebConfig_VersionName'=>'yunpai.sindns.com',//版权信息
+    'APP_STATUS' => 'debug',//应用调试模式状态
+    'WebConfig_Url'=>'http://yunpai.sindns.com',//网站访问地址
+];
+// +----------------------------------------------------------------------
+// | 网站基本信息配置  end
+// +----------------------------------------------------------------------
+//===============系统应用配置信息begin==================
+$newArray['yubiaoqian']=array(
+    'app_id'=>'2019031863612029',//应用AppID
+    'rsaPublicKeyPem'=>'xxxxxxx',//应用公钥
+    'rsaPrivateKey'=>'xxxxxxxxx',//请填写商户私钥，一行字符串
+    'alipayrsaPublicKey' => 'xxxxxx',//请填写支付宝公钥，一行字符串
+    'url'=>'https://openapi.alipay.com/gateway.do',
+);
+//===============系统应用配置信息 end===================
+
 return $newArray;
