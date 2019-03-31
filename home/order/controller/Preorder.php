a7d45d411b659ed5b5f8c722eb113ce111f1da6a
EDZ-20180516PBB\Edianzu <814418177@qq.com>
2019-03-31 17:17:15 +0800

    更新保存

--- home/order/controller/Preorder.php
+++ home/order/controller/Preorder.php
@@ -56,7 +56,81 @@ class Preorder extends CommonBase
      * @desc: 提交订单
      */
     public function suborder(){
-        print_r($_POST);
+        $param = $this->request->post();
+        $memberid=intval(isset($param['memberid']) ? trim($param['memberid']) : '0');
+        $templateId=intval(isset($param['templateId']) ? trim($param['templateId']) : '0');
+        $printNum=intval(isset($param['printNum']) ? trim($param['printNum']) : '0');
+        $printMode=intval(isset($param['printMode']) ? trim($param['printMode']) : '0');
+        $printSort=intval(isset($param['printSort']) ? trim($param['printSort']) : '0');
+        $paperId=intval(isset($param['paperId']) ? trim($param['paperId']) : '0');
+        $gettime=time();
+
+        if(!$memberid or !$printNum or !$templateId or !$paperId ){
+            echo '<script language="javascript">alert("参数错误！");history.go(-1);</script>';
+            exit;
+        }
+
+        $ModelMember=Db::name('member');
+        $ModelTemplate=Db::name('template');
+        $ModelTemplateContent=Db::name('template_content');
+        $ModelPaper=Db::name('paper');
+
+        $getoneMember=$ModelMember->where("member_id='$memberid'")->find();
+        $getoneTemplate=$ModelTemplate->where("template_id='$templateId'")->find();
+        $getoneTemplateContent=$ModelTemplateContent->where("template_id='$templateId'")->find();
+        $getonePaper=$ModelPaper->where("id='$paperId'")->find();
+        if(empty($getoneMember) or empty($getoneTemplate) or empty($getoneTemplateContent) or empty($getonePaper) ){
+            echo '<script language="javascript">alert("参数错误！");history.go(-1);</script>';
+            exit;
+        }
+        //计算费用
+        $_priceTotal = 0.00;
+        $_price = 0.00;
+        // 用户自定义的纸张，后台手动计算价格
+        if($getonePaper['data_type'] == 1){
+            $_price = 0.00;
+        }else{
+            if($printNum > 500){
+                $_price = $getonePaper['price_three'];
+            }else if($printNum > 100){
+                $_price = $getonePaper['price_two'];
+            }else{
+                $_price = $getonePaper['price_one'];
+            }
+        }
+        // 计算总费用
+        $_priceTotal=price($printNum*$_price,2);
+
+        // 开启事务
+        Db::startTrans();
+        //Db::rollback();//事务回滚
+        // 1\insert order 订单表==写入订单表
+        $dataOrder=array('order_no'=>$order_no,
+            'template_id'=>$getoneTemplate['template_id'],
+            'template_title'=>$getoneTemplate['title'],
+            'print_num'=>$printNum,
+            'price'=>$_priceTotal,
+            'name'=>$getoneMember['real_name'],
+            'email'=>$getoneMember['email'],
+            'moblie'=>$getoneMember['mobile'],
+            'score_pay'=>$_priceTotal,
+            'score_real_pay'=>$_priceTotal,
+            'member_id'=>$getoneMember['member_id'],
+            'username'=>$getoneMember['username'],
+            'pay_flag'=>'1',
+            'paper_size_long'=>$getoneTemplateContent['paper_size_long'],
+            'paper_size_wide'=>$getoneTemplateContent['paper_size_wide'],
+            'paper_size_unit'=>$getoneTemplateContent['paper_size_unit'],
+            'paper_direction'=>$getoneTemplateContent['paper_direction'],
+            'print_mode'=>$printMode,
+            'print_sort'=>$printSort,
+            'down_pdf'=>$getoneTemplate['pdf'],
+            'create_time'=>$gettime,
+            'update_time'=>$gettime
+            );
+
+        //提交事务
+        Db::commit();
         exit;
     }
 }
