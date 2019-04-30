ca66539265eadda26ae1fcb5d626d2ea266ccdc5
赵黎明 <2598394868@qq.com>
2019-04-30 17:15:35 +0800

    '微信公众号列表'

--- business/data/controller/Data.php
+++ business/data/controller/Data.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\data\controller;
 use think\Controller;
+use think\Db;
+use think\Session;
 
 /**
  * Created by PhpStorm.
@@ -16,9 +18,29 @@ class Data extends Controller
      */
     public function index()
     {
-        //查询扫码数量
-        $content = file_get_contents('http://suyuan.localhost/admin.php/sinterface/dcolligate/index');
-        $this->assign('content',$content);
+        //查询扫码记录
+        $product = Db::name('product ')
+            ->alias('a')
+            ->join('product_code_info_visit_record w', 'a.product_id = w.product_id')
+            ->join('admin s', 's.admin_id = a.admin_id')
+            ->field('name,title')
+            ->select();
+        $count = count($product);
+        //兑换的产品数量数据
+        $id = Session::get('adminid');
+        $con = Db::name('integral_order')->where('admin_id',$id)->field('order_no')->select();
+        $count2 = count($con);
+//        dump($count2);die;
+        $this->assign('count',$count);
+        $this->assign('count2',$count2);
         return $this->fetch();
     }
+
+    public function add()
+    {
+        $arr['x'] = [1,2,3,4,5];
+        $arr['y'] = [1,2,3,4,5];
+        return json($arr);
+    }
+
 }
--- business/public_address/controller/Wpa.php
+++ business/public_address/controller/Wpa.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\public_address\controller;
 use think\Controller;
+use think\Db;
+use think\Session;
 
 /**
  * Created by PhpStorm.
@@ -16,6 +18,31 @@ class Wpa extends Controller
      */
     public function index()
     {
+        //查询已有公众号
+        $id = Session::get('adminid');
+        $weChap = Db::name('wechat_menu')->where('admin_id',$id)->select();
+        $this->assign('weChap',$weChap);
+        return $this->fetch();
+    }
+
+    /**
+     * @return 查看公众号详情
+     */
+    public function info()
+    {
+        $this->error('未完成');die;
+        return $this->fetch();
+    }
+
+    /**
+     * @return 管理设置
+     */
+    public function set()
+    {
+        $param = $this->request->param();
+        $id = htmlspecialchars(isset($param['id']) ? $param['id'] : '');
+        $con = Db::name('wechat_menu')->where('id',$id)->find();
+        $this->assign('content',$con);
         return $this->fetch();
     }
 }
