76636acca47e1bfa642fb4b0ead286ca4411fe0b
赵黎明 <2598394868@qq.com>
2019-05-09 08:38:43 +0800

    '支付信息'

--- business/money/controller/Pay.php
+++ business/money/controller/Pay.php
@@ -15,11 +15,17 @@ class Pay extends Controller
 {
     public function pagePay()
     {
-        //Loader::import('demo.index',EXTEND_PATH,'.php');
-        $WIDsubject='标题';
-        $this->assign("WIDout_trade_no",'订单号');
+       // Loader::import('onlinepay.alipay.index',EXTEND_PATH,'.php');
+        //获取数据
+        $param = $this->request->param();
+        $WIDout_trade_no = $param['WIDout_trade_no'];
+        $WIDsubject = $param['WIDsubject'];
+        $WIDtotal_amount = $param['WIDtotal_amount'];
+//        dump($WIDsubject);die;
+//        $WIDsubject='标题';
+        $this->assign("WIDout_trade_no",$WIDout_trade_no);
         $this->assign("WIDsubject",$WIDsubject);
-        $this->assign("WIDtotal_amount",'0.01');
+        $this->assign("WIDtotal_amount",$WIDtotal_amount);
         $this->assign("WIDbody",'描述');
         return $this->fetch('preorder/suborder');
     }
